7cf1a3eb8654e459ce3ade0d373dc98f58c56f19
Nabin Karki <naabin@outlook.com>
2019-09-23 20:31:54 +1000

    Hacker rank practice

---
+++ HackerRank/src/com/practice/IPRegex.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class IPRegex {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+		in.close();
+	}
+}
+
+final class MyRegex{
+	String pattern =  "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
